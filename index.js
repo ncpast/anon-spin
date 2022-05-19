fd3cec9671744bd663cfe74f4bd93f05ae1b0a89
ncpast <svetlisasha@gmail.com>
2022-05-19 22:29:27 +0300

    ye

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const frames = fs.readFileSync('./frames.txt', { encoding: 'utf-8', flag: 'r' }).split('8');
+
+(function render(index) {
+    setTimeout(() => {
+        console.clear();
+        process.stdout.write('\r' + frames[index]);
+        index++;
+        if (index >= frames.length) return render(0);
+        render(index);
+    }, 200);
+})(0);
